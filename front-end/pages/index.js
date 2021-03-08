c15a82739760ae893a32cb76b37c73239d913e88
PatrickGTR <patricksubang@live.com>
2021-03-08 00:20:57 +0000

    using promise.all to avoid blocking calls

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -8,8 +8,11 @@ import SideBar from "../components/sidebar";
 import Layout from "../components/layout";
 import Link from "next/link";
 import sendRequest from "../utils/sendRequest";
+import { noAvailableServer } from "../utils/message";
 
-const Home = ({ stats, posts }) => {
+const Home = ({ stats, posts, media }) => {
+  const { hottest, newest } = media;
+
   return (
     <Layout title="Home">
       <div className="row">
@@ -64,6 +67,41 @@ const Home = ({ stats, posts }) => {
 
         <div className="column">
           <SideBar stats={stats} />
+
+          <h3>Media</h3>
+          <div className="row">
+            <div className="column">
+              <h3>🔥</h3>
+              <img
+                className="media-thumbnail"
+                src={`https://img.youtube.com/vi/${
+                  hottest?.youtubeLink.split("=")[1] || `2Dn8toO7tKs`
+                }/mqdefault.jpg`}
+              />
+
+              <br />
+              <span>
+                <strong>{hottest?.title || noAvailableServer}</strong>
+              </span>
+              <br />
+              <span>{hottest?.author || noAvailableServer}</span>
+            </div>
+            <div className="column">
+              <h3>🆕</h3>
+              <img
+                className="media-thumbnail"
+                src={`https://img.youtube.com/vi/${
+                  newest?.youtubeLink.split("=")[1] || `2Dn8toO7tKs`
+                }/mqdefault.jpg`}
+              />
+              <br />
+              <span>
+                <strong>{newest?.title || noAvailableServer}</strong>
+              </span>
+              <br />
+              <span>{newest?.author || noAvailableServer}</span>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
@@ -87,42 +125,95 @@ export const getStaticProps = async () => {
   });
 
   let serverStats = {};
+  let trendingMedia = {};
+
+  // Promise.all impl
   try {
-    let response, data;
-    // fetch highest kills
-    response = await sendRequest("GET", "server/stats?type=1&option=1");
-    data = await response.json();
-    serverStats = { ...serverStats, highestKill: data.value };
+    const killAPICall = sendRequest("GET", "server/stats?type=1&option=1");
+    const moneyAPICall = sendRequest("GET", "server/stats?type=1&option=2");
+    const deathAPICall = sendRequest("GET", "server/stats?type=1&option=3");
+    const totalAccountsAPICall = sendRequest("GET", "server/stats?type=2");
+    const newestMediaAPICall = sendRequest("GET", "media/trending?q=newest");
+    const hottestMediaAPICall = sendRequest("GET", "media/trending?q=hottest");
 
-    // fetch highest money
-    response = await sendRequest("GET", "server/stats?type=1&option=2");
-    data = await response.json();
-    serverStats = { ...serverStats, highestMoney: data.value };
+    const values = await Promise.all([
+      killAPICall,
+      moneyAPICall,
+      deathAPICall,
+      totalAccountsAPICall,
+      newestMediaAPICall,
+      hottestMediaAPICall,
+    ]);
 
-    // fetch highest deaths
-    response = await sendRequest("GET", "server/stats?type=1&option=3");
-    data = await response.json();
+    const [
+      highestKill,
+      highestMoney,
+      highestDeaths,
+      totalAccounts,
+      newestMedia,
+      hottestMedia,
+    ] = await Promise.all(values.map((value) => value.json()));
+
     serverStats = {
-      ...serverStats,
-      highestDeaths: data.value,
+      highestKill: highestKill.value,
+      highestMoney: highestMoney.value,
+      highestDeaths: highestDeaths.value,
+      playerCount: totalAccounts.value,
     };
 
-    //fetch total accounts
-    response = await sendRequest("GET", "server/stats?type=2");
-    data = await response.json();
-    serverStats = { ...serverStats, playerCount: data.value };
-  } catch (e) {
-    // write a proper logging
-    console.log(
-      "an error was caught trying to fetch server data, please check logs",
-      e,
-    );
+    trendingMedia = {
+      newest: newestMedia,
+      hottest: hottestMedia,
+    };
+  } catch (err) {
+    console.log(`promise.all ${err}`);
   }
+  // try {
+  //   let response, data;
+  //   // fetch highest kills
+  //   response = await sendRequest("GET", "server/stats?type=1&option=1");
+  //   data = await response.json();
+  //   serverStats = { ...serverStats, highestKill: data.value };
+
+  //   // fetch highest money
+  //   response = await sendRequest("GET", "server/stats?type=1&option=2");
+  //   data = await response.json();
+  //   serverStats = { ...serverStats, highestMoney: data.value };
+
+  //   // fetch highest deaths
+  //   response = await sendRequest("GET", "server/stats?type=1&option=3");
+  //   data = await response.json();
+  //   serverStats = {
+  //     ...serverStats,
+  //     highestDeaths: data.value,
+  //   };
+
+  //   //fetch total accounts
+  //   response = await sendRequest("GET", "server/stats?type=2");
+  //   data = await response.json();
+  //   serverStats = { ...serverStats, playerCount: data.value };
+
+  //   // Media
+  //   response = await sendRequest("GET", "media/trending?q=newest");
+  //   data = await response.json();
+  //   trendingMedia = { newest: data };
+
+  //   response = await sendRequest("GET", "media/trending?q=hottest");
+  //   data = await response.json();
+  //   trendingMedia = { ...trendingMedia, hottest: data };
+  // } catch (e) {
+  //   // write a proper logging
+  //   console.log(
+  //     "an error was caught trying to fetch server data, please check logs",
+  //     e,
+  //   );
+  // }
 
   return {
     props: {
       posts,
       stats: serverStats,
+      media: trendingMedia,
     },
   };
 };
